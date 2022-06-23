dd4b6769527f4d6c2e51b0bf37932908f4876693
kalpitbhanawat <49224317+kalpitbhanawat@users.noreply.github.com>
2022-06-23 08:48:10 +0530

    Add files via upload

---
+++ Compile_Time_Polymorphism.java
@@ -0,0 +1,28 @@
+public class Compile_Time_Polymorphism {
+    public static void main(String[] args)
+    {
+
+        // Calling method by passing
+        // input as in arguments
+        Calculate c=new Calculate();
+        System.out.println(c.Multiply(2, 4));
+        System.out.println(c.Multiply(5.5, 6.3));
+    }
+}
+class Calculate{
+    static int Multiply(int a, int b)
+    {
+
+        // Returns product of integer numbers
+        return a * b;
+    }
+
+    // Method 2
+    // With same name but with 2 double parameters
+    static double Multiply(double a, double b)
+    {
+
+        // Returns product of double numbers
+        return a * b;
+    }
+}
